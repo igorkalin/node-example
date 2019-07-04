f177fa424b29e50e7ef56513dea7839f3586de9d
Igor Kalin <igork994@gmail.com>
2019-07-04 10:16:37 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req,res)=>{
+    res.send('Hello World');
+});
+
+//PORT
+
+const port = process.env.PORT || 3000;
+
+app.get('/api/courses', (req,res)=>{
+    res.send([1,2,3]);
+});
+app.listen(port, ()=> console.log(`Listening on port ${port}....`));
