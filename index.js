52ca607819a614d552d7f496ee331974f417158d
igorkalin <igork994@gmail.com>
2019-07-05 12:07:46 +0200

    final rest api

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+
+const Joi = require('joi');
+
 const express = require('express');
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req,res)=>{
     res.send('Hello World');
@@ -21,16 +26,68 @@ app.get('/api/courses/:id', (req , res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
 
     if(!course) 
-        res.status(404).send('Course with given ID was not found!');
+       return res.status(404).send('Course with given ID was not found!');
 
     res.send(course);
 });
 
 
-app.get('/api/posts/:year/:month', (req , res) =>{
-    res.send(req.query);
+
+app.post('/api/courses', (req,res)=>{
+
+   const { error }  = validateCourse(req.body);
+
+    if (error)
+        return res.status(400).send(error.details[0].message);
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
 });
 
+app.put('/api/courses/:id', (req,res)=>{
+    //Look up the course
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    //If not existing, return 404,
+    if(!course) 
+       return res.status(404).send('Course with given ID was not found!');
+    //Validate
+    //const result = validateCourse(req.body);
+    const { error } = validateCourse(req.body); //result.error
+
+    //If invalid, return 404
+    if (error)
+        return res.status(400).send(error.details[0].message);
+
+    //Update course
+    course.name = req.body.name;
+    //Return the updated course
+    res.send(course);
+});
+
+function validateCourse(course){
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(course, schema);
+}
+
+app.delete('/api/courses/:id', (req,res)=>{
+    //Look up the course
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    //If not existing, return 404,
+    if(!course) 
+        return res.status(404).send('Course with given ID was not found!');
+    //Delete
+    const index = courses.indexOf(course);
+    courses.splice(index,1);
+    //Return the same course
+    res.send(course);
+});
 //PORT
 const port = process.env.PORT || 3000;
-app.listen(port, ()=> console.log(`Listening on port ${port}....`));
+app.listen(port, ()=> console.log(`Listening on port ${port}....`)); 
